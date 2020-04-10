8790fadceab45804a0736dbbf76dd98db4e73875
pinkgoatcheese <62079296+pinkgoatcheese@users.noreply.github.com>
2020-04-09 19:04:00 -0500

    Cron
    fixed problems related to crontab
    $username is local user account name

--- blocks.php
+++ blocks.php
@@ -1,3 +1,4 @@
+#!/usr/bin/php
 <?php
 
 // script requires php7.2, curl,  bcmath
@@ -8,8 +9,9 @@ use \Arweave\SDK\Arweave;
 use \Arweave\SDK\Support\Wallet;
 
 // get local block height
-$last_local_block = trim(file_get_contents('blocks.txt'));
-
+$last_local_block = file_get_contents('/home/$username/record-the-bch-blockchain/blocks.txt');
+rtrim($last_local_block, '\n');
+echo($last_local_block);
 // get block count from bitcoin.com
 get_block_count($last_local_block);
 
@@ -24,32 +26,36 @@ function get_block_count($last_local_block)
     ));
     $block_count = curl_exec($curl); // Send the request, save the response
     curl_close($curl); // Close request
+    echo($block_count);
+    printf("\n");
+    // echo($last_local_block);
     get_missing_blocks($block_count, $last_local_block);
 }
 
 // Retrieve missing block information to save if needed
 function get_missing_blocks($block_count, $last_local_block)
 {
-    if ($block_count == $last_local_block)
+    if ($block_count === $last_local_block)
     {
-        // Print message to logfile if no new blocks to retrieve
         printf("\n");
-        echo("BCH Block Height is equal to last local block");
+        printf("BCH Block Height = to last local block");
         exit();
     }
     elseif ($block_count > $last_local_block)
     {
         foreach (range($last_local_block, $block_count) as $block_number) {
             // increment local block number by one
-            $last_local_block++;
-            if ($last_local_block == $block_count)
+            if ($block_count === $block_number)
             {
-                // New line and notify of job completion in log
                 printf("\n"); 
                 echo("All missing blocks saved to arweave");
                 exit();
             }
-            $url = "https://rest.bitcoin.com/v2/block/detailsByHeight/" . $last_local_block . "?verbose=true";
+            // output block number to log file
+            // print_r($last_local_block);
+            echo($block_number);
+            $url = "https://rest.bitcoin.com/v2/block/detailsByHeight/" . $block_number . "?verbose=true";
+            echo($url);
             $curl = curl_init();
             curl_setopt_array($curl, array(
             CURLOPT_URL => $url,            // set the request URL
@@ -64,7 +70,7 @@ function get_missing_blocks($block_count, $last_local_block)
             $block_hash = $data_response_array['hash'];
             $previous_block = $data_response_array['previousblockhash'];
             $block_time = $data_response_array['time'];
- 
+            printf($block_hash); 
             // Get Block Header to Save
             $header_url = "https://rest.bitcoin.com/v2/blockchain/getBlockHeader/" . $block_hash . "?verbose=false";
             // echo($header_url);
@@ -76,28 +82,29 @@ function get_missing_blocks($block_count, $last_local_block)
             ));
             $header_response = curl_exec($curl); // Send the request, save the response
             curl_close($curl); // Close request
+            // echo($header_response);
+
             // convert header response to binary
             $hex_input = str_replace('"', '', $header_response);
             gettype($header_response);
             $header_data_array = hex2ByteArray($hex_input);
+            //print_r($header_data);
+            // validatae data
             $binary_header = pack('C*', ...$header_data_array);
-            //validate data
+
             if (!is_array($data_response_array))
             {
-                // New line and output error to logfile
                 printf("\n");
                 echo('Data Response is not an array');
                 exit();
             }
             elseif (!isset($block_height))
             {
-               // New line and output error to logfile
                printf("\n");
                echo('Empty Variable for block_height exiting');
                exit();
             } else {
-                // save block number locally then save
-                file_put_contents('blocks.txt', $block_height);
+                // save
                 save_to_arweave($block_height, $block_hash, $previous_block, $block_time, $binary_header);
             }
         }
@@ -116,7 +123,7 @@ function save_to_arweave($block_height, $block_hash, $previous_block, $block_tim
     // It contains the public methods for creating, sending and getting transactions
     $arweave = new \Arweave\SDK\Arweave('https', 'arweave.net', '443');
     // Decode our JWK file to a PHP array named $jwk
-    $jwk = json_decode(file_get_contents('jwk.json'), true);
+    $jwk = json_decode(file_get_contents('/home/$username/record-the-bch-blockchain/jwk.json'), true);
     // Create a new wallet using the $jwk array
     $wallet =  new \Arweave\SDK\Support\Wallet($jwk);
     // Create a new ARWEAVE transaction to store the verified data
@@ -138,6 +145,7 @@ function save_to_arweave($block_height, $block_hash, $previous_block, $block_tim
     printf("\n");
     // Send the transaction to the arweave network
     $arweave->api()->commit($transaction);
-    // Output the saved block height to logfile
-    echo($block_height);
+    printf("\n");
+    ++$block_height;
+    file_put_contents('/home/$username/record-the-bch-blockchain/blocks.txt', $block_height);
 }
